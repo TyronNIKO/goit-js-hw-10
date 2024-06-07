b864917059ef70330303ee26024f3a4c72f253d6
Volodymyr Bilohorodskyi <niko.fm@gmail.com>
2024-06-07 07:47:59 +0300

    Task 1 done

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -10,6 +10,71 @@ import iziToast from 'izitoast';
 // Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
 
+class Timer {
+	refs = {
+		input: document.querySelector('input#datetime-picker'),
+		start: document.querySelector('[data-start]'),
+		days: document.querySelector('[data-days]'),
+		hours: document.querySelector('[data-hours]'),
+		minutes: document.querySelector('[data-minutes]'),
+		seconds: document.querySelector('[data-seconds]'),
+	};
+	constructor() {}
+	convertMs(ms) {
+		// Number of milliseconds per unit of time
+		const second = 1000;
+		const minute = second * 60;
+		const hour = minute * 60;
+		const day = hour * 24;
+
+		// Remaining days
+		const days = Math.floor(ms / day);
+		// Remaining hours
+		const hours = Math.floor((ms % day) / hour);
+		// Remaining minutes
+		const minutes = Math.floor(((ms % day) % hour) / minute);
+		// Remaining seconds
+		const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+		return { days, hours, minutes, seconds };
+	}
+	validateTime(current, selected) {
+		let valid = false;
+		if (current < selected) {
+			valid = true;
+		}
+		return valid;
+	}
+	addLeadingZero(value) {
+		console.log(value);
+		// let formated = String(value).padStart(2, '0');
+		let formated = value < 10 ? '0' + value : value;
+		return formated;
+	}
+	// renderTimer({ days, hours, minutes, seconds }) {
+	// 	this.refs.days.innerText = this.addLeadingZero(days);
+	// 	this.refs.hours.innerText = this.addLeadingZero(hours);
+	// 	this.refs.minutes.innerText = this.addLeadingZero(minutes);
+	// 	this.refs.seconds.innerText = this.addLeadingZero(seconds);
+	// }
+	renderTimer(obj) {
+		const keys = Object.keys(obj);
+		keys.forEach(key => {
+			this.refs[key].innerText = this.addLeadingZero(obj[key]);
+		});
+	}
+	init() {
+		this.refs.start.setAttribute('disabled', '');
+		this.refs.start.addEventListener('click', e => {
+			e.preventDefault();
+			console.log(e.target);
+			const currDate = Date.now();
+			console.log(currDate);
+		});
+	}
+}
+const timer = new Timer();
+
 const options = {
 	enableTime: true,
 	time_24hr: true,
@@ -17,61 +82,24 @@ const options = {
 	minuteIncrement: 1,
 	onClose(selectedDates) {
 		console.log(selectedDates[0]);
-		validateTime(this.defaultDate, selectedDates[0]);
+		const validation = timer.validateTime(
+			options.defaultDate,
+			selectedDates[0]
+		);
+		if (validation) {
+			const diff = selectedDates[0] - options.defaultDate;
+			timer.renderTimer(timer.convertMs(diff));
+		} else {
+			iziToast.error({
+				position: 'topRight', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter
+				title: 'Error',
+				message: 'Please choose a date in the future',
+			});
+		}
 	},
 };
 
-const refs = {
-	input: document.querySelector('input#datetime-picker'),
-	start: document.querySelector('[data-start]'),
-	days: document.querySelector('[data-days]'),
-	hour: document.querySelector('[data-hour]'),
-	minutes: document.querySelector('[data-minutes]'),
-	seconds: document.querySelector('[data-seconds]'),
-};
-console.log(refs);
+timer.init();
 
-const picker = flatpickr(refs.input, options);
+const picker = flatpickr(timer.refs.input, options);
 console.log(picker);
-
-refs.start.setAttribute('disabled', '');
-refs.start.addEventListener('click', e => {
-	e.preventDefault();
-	console.log(e.target);
-	const currDate = Date.now();
-	console.log(currDate);
-});
-
-function convertMs(ms) {
-	// Number of milliseconds per unit of time
-	const second = 1000;
-	const minute = second * 60;
-	const hour = minute * 60;
-	const day = hour * 24;
-
-	// Remaining days
-	const days = Math.floor(ms / day);
-	// Remaining hours
-	const hours = Math.floor((ms % day) / hour);
-	// Remaining minutes
-	const minutes = Math.floor(((ms % day) % hour) / minute);
-	// Remaining seconds
-	const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-	return { days, hours, minutes, seconds };
-}
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
-function validateTime(current, selected) {
-	console.log('validateTime');
-
-	if (current > selected) {
-		iziToast.error({
-			title: 'Error',
-			message: 'Please choose a date in the future',
-		});
-	}
-}
